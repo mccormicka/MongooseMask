81dd5fea5162629e48af321eceac13ede1f89a2b
Anthony McCormick <=>
2013-09-15 20:58:47 +0100

    Adde Expose method to mask so that you can explicitly expose values instead of masking them

--- index.js
+++ index.js
@@ -19,13 +19,7 @@ var _ = require('lodash');
 exports = module.exports = function (values) {
     function wrap(original) {
         return function (obj) {
-            var duplicate = obj;
-            if(_.isFunction(obj.toObject)){
-                duplicate = obj.toObject();
-                _.each(values, function(value){
-                    delete duplicate[value];
-                });
-            }
+            var duplicate = module.exports.mask(obj, values);
             original(duplicate);
         };
     }
@@ -35,4 +29,52 @@ exports = module.exports = function (values) {
         res.jsonp = wrap(res.jsonp.bind(res));
         next();
     };
+};
+
+/**
+ * Expose all values on an object except for the masked items
+ * passed in the values array.
+ * @param obj
+ * @param values
+ * @returns {*}
+ */
+module.exports.mask = function mask(obj, values) {
+    var duplicate = obj;
+    if (_.isFunction(obj.toObject)) {
+        duplicate = obj.toObject();
+        _.each(values, function (value) {
+            delete duplicate[value];
+        });
+    }
+    return duplicate;
+};
+
+/**
+ * Mask all values on a given object except for those that
+ * are explicitly exposed through the values array. The value
+ * can be a String which will directly be a one-to-one mapping
+ * for example
+ *     _id -> _id will expose the _id mongoose value
+ * or an object that maps keys to values
+ * for example
+ *     {_id:'id'} will expose _id as id on the object.
+ * [ '_id', {_id:'id} ] will expose both id and _id
+ * @param obj
+ * @param values
+ * @returns {{}}
+ */
+module.exports.expose = function expose(obj, values){
+    var duplicate = {};
+    if (_.isFunction(obj.toObject)) {
+        _.each(values, function (item) {
+            if(_.isObject(item)){
+                _.forIn(item, function(value, key){
+                    duplicate[value] = obj[key];
+                });
+            }else{
+                duplicate[item] = obj[item];
+            }
+        });
+    }
+    return duplicate;
 };
--- test/Index.spec.js
+++ test/Index.spec.js
@@ -31,12 +31,13 @@ describe('MongooseMask Tests', function () {
     });
 
     describe('SHOULD', function () {
+
         it('Be able to acquire index', function () {
             var test = require('../index');
             expect(test).not.toBeNull();
         });
 
-        it('Return all fields when no mask applied', function (done) {
+        it('Return all fields when no mask applied middleware', function (done) {
             var app = express();
             app.get('/model', function (req, res) {
                 Model.findOne({}, function(err, result){
@@ -56,10 +57,52 @@ describe('MongooseMask Tests', function () {
                     done(err);
                 });
         });
+
+        it('Return all fields if no mask applied', function (done) {
+            Model.findOne({}, function(err, result){
+                expect(result._id).toBeDefined();
+                expect(result.publicField).toBe('Hello');
+                expect(result.privateField).toBe('World');
+                done(err);
+            });
+        });
+
+        it('Mask fields if mask applied', function (done) {
+            Model.findOne({}, function(err, result){
+                result = mongooseMask.mask(result, ['_id', 'privateField']);
+                expect(result._id).toBeUndefined();
+                expect(result.publicField).toBe('Hello');
+                expect(result.privateField).toBeUndefined();
+                done(err);
+            });
+        });
+
+
+        it('Be able to expose explicit fields', function (done) {
+            Model.findOne({}, function(err, result){
+                result = mongooseMask.expose(result, ['_id', 'privateField']);
+                expect(result._id).toBeDefined();
+                expect(result.publicField).toBeUndefined();
+                expect(result.privateField).toBeDefined();
+                done(err);
+            });
+        });
+
+        it('Be able to rename exposed fields', function (done) {
+            Model.findOne({}, function(err, result){
+                result = mongooseMask.expose(result, [{'_id' : 'banana'}, 'privateField']);
+                expect(result._id).toBeUndefined();
+                expect(result.banana).toBeDefined();
+                expect(result.publicField).toBeUndefined();
+                expect(result.privateField).toBeDefined();
+                done(err);
+            });
+        });
     });
 
     describe('SHOULD NOT', function () {
-        it('Return all fields when mask applied', function (done) {
+
+        it('Return all fields when mask applied middleware', function (done) {
             var app = express();
             app.use(mongooseMask(['privateField', '_id']));
 
